c006ba26840732f854a8d5ded8aa1feb12e9a4a1
5730289021-NN <norawit.n@student.chula.ac.th>
2017-03-21 16:50:48 +0700

    add comments

--- WaterMeasure.ino
+++ WaterMeasure.ino
@@ -24,32 +24,32 @@ void setup() {
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
 
   Timer1.initialize(1000000);
-  Timer1.attachInterrupt(transmitSerial); // blinkLED to run every 0.15 seconds
+  Timer1.attachInterrupt(transmitSerial); // after 1 second,it will perform 'transmitSerial' function
 }
 
 void transmitSerial()
 {
-  Serial.println(sensorValue);
+  Serial.println(sensorValue); // show value of water level in Serial Monitor
 }
 
 void loop() {
   // read the value from the sensor:
-  if(Serial.available()){
+  if(Serial.available()){            // read 1 or 0 from serial monitor and define mode
     int serialRead = Serial.read();
     if(serialRead == 48)
     {
-      mode = 0;
+      mode = 0;                     // stop showing the value of water level
     }else if(serialRead == 49)
     {
-      mode = 1;
+      mode = 1;                     // show the value of water level
     }
     //Serial.println(serialRead);
   }
   
-  sensorValue = (mode==0) ? 0 : analogRead(sensorPin);
-  
-  sevseg.setNumber(sensorValue,0);
-  sevseg.refreshDisplay();
+  sensorValue = (mode==0) ? 0 : analogRead(sensorPin); // if water level is 0, sensorValue = 0
+                                                       // else sensorValue = value from pin A0
+  sevseg.setNumber(sensorValue,0); // display value on seven segment from sensorValue
+  sevseg.refreshDisplay();         // refresh displayed value for show new value
   if(sensorValue < 250)
   {
     digitalWrite(10,LOW);
